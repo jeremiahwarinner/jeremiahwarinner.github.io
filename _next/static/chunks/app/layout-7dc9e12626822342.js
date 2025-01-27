228f0eb825daf8a6c7d8a06e9e0b4a73305a1410
jeremiah <jeremiah@luday.se>
2025-01-26 21:32:51 -0600

    new site

---
+++ _next/static/chunks/app/layout-7dc9e12626822342.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{5937:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,266,23)),Promise.resolve().then(l.t.bind(l,347,23))},347:()=>{},266:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_d65c78"}}},e=>{var s=s=>e(e.s=s);e.O(0,[408,441,517,358],()=>s(5937)),_N_E=e.O()}]);
